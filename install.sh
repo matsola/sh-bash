18d33d6a93023314509b2925df0466d1e116fb20
Mats-Ola Persson <matsola.persson@gmail.com>
2022-12-30 01:43:27 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+sh_bash_dir=$(dirname $(realpath $0))
+
+mkbackupandlink () {
+    file=$1
+    if [ -f $HOME/$file ] || [ -L $HOME/$file ] ; then
+        echo "Backing up $file to $file-backup-$(date +'%y%m%d%H%M%S')"
+        mv $HOME/$file $HOME/$file-backup-$(date +'%y%m%d%H%M%S')
+    fi
+
+    ln -s $sh_bash_dir/$file $HOME/$file
+}
+
+git submodule update --init --recursive
+
+mkbackupandlink ".bashrc"
+mkbackupandlink ".bash_profile"
+mkbackupandlink ".git-completion.bash"
+mkbackupandlink ".bash-git-prompt"
